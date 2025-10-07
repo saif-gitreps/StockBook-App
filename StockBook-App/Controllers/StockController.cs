16c58c0e863027643e7be4624eb5450f4126cb99
Saif <droconos99@gmail.com>
2025-10-08 00:00:03 +0600

    Add: Stock Controller

---
+++ StockBook-App/Controllers/StockController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StockBook_App.Data;
+
+namespace StockBook_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public StockController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+        }
+
+        [HttpGet("")]
+
+    }
+}
