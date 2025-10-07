ba471cb0403b66aba3dcb84471c48e5299bcaf5d
Saif <droconos99@gmail.com>
2025-10-07 21:11:53 +0600

    Add project files.

---
+++ StockBook-App/Models/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace StockBook_App.Models.Entities
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+
+
+        public Guid? StockId { get; set; }
+
+        // Navigation property to the related Stock entity
+        public Stock? Stock { get; set; }
+    }
+}
---
+++ StockBook-App/Models/Entities/Stock.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockBook_App.Models.Entities
+{
+    public class Stock
+    {
+        public Guid Id { get; set; }
+        public required string Symbol { get; set; }
+
+        public required string CompanyName { get; set; }
+
+        [Column(TypeName = "decimal(18,4)")]
+        public required decimal Purchase { get; set; }
+
+        [Column(TypeName = "decimal(18,4)")]
+        public required decimal LastDiv { get; set; }
+        public required string Industry { get; set; }
+        public required long MarketCap { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+    }
+}
