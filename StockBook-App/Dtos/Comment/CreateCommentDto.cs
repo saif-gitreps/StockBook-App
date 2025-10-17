911eb0a2e100236b3d674c9839462636863d279f
Saif <droconos99@gmail.com>
2025-10-17 18:35:28 +0600

    Add: Comment request body validation

--- StockBook-App/Dtos/Comment/CreateCommentDto.cs
+++ StockBook-App/Dtos/Comment/CreateCommentDto.cs
@@ -1,9 +1,20 @@
-﻿namespace StockBook_App.Dtos.Comment
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockBook_App.Dtos.Comment
 {
     public class CreateCommentDto
     {
+        [Required]
         public required Guid StockId { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Title must be at least 5 characters long.")]
+        [MaxLength(256, ErrorMessage = "Title cannot be longer than 256 characters.")]
         public required string Title { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Content must be at least 5 characters long.")]
+        [MaxLength(256, ErrorMessage = "Content cannot be longer than 256 characters.")]
         public required string Content { get; set; }
 
     }
--- StockBook-App/Dtos/Comment/UpdateCommentRequestDto.cs
+++ StockBook-App/Dtos/Comment/UpdateCommentRequestDto.cs
@@ -1,8 +1,16 @@
-﻿namespace StockBook_App.Dtos.Comment
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockBook_App.Dtos.Comment
 {
     public class UpdateCommentRequestDto
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Title must be at least 5 characters long.")]
+        [MaxLength(256, ErrorMessage = "Title cannot be longer than 256 characters.")]
         public required string Title { get; set; }
+        [Required]
+        [MinLength(5, ErrorMessage = "Content must be at least 5 characters long.")]
+        [MaxLength(256, ErrorMessage = "Content cannot be longer than 256 characters.")]
         public required string Content { get; set; }
     }
 }
